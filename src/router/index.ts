cf8209b8a9c9829231321e03ee577c1e97138866
junminhong <junminhong1110@gmail.com>
2021-12-14 23:07:18 +0800

    清除初始化專案產生的demo code，新增alltickets路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
+import AllTicket from "../views/AllTicket.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -15,6 +16,15 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/alltickets",
+    name: "AllTickets",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AllTicket.vue"),
   },
 ];
 
