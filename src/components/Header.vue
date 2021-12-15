1fad136c6e9436fba5a21c61c9d963f8d75fb7f0
junminhong <junminhong1110@gmail.com>
2021-12-15 14:06:08 +0800

    Header新增cart icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,24 +3,29 @@
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                 <a href="/#/alltickets" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
-                    <span class="fs-2"><i class="bi bi-emoji-laughing bi me-2"></i>趣訂票</span>
+                    <span class="fs-2"><i class="bi bi-emoji-laughing me-2"></i>趣訂票</span>
                 </a>
-
                 <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-                <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+                    <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
                 </form>
-
+                
+                <button type="button" class="btn btn-light  position-relative me-4">
+                    <i class="bi bi-cart4 fs-4"></i>購物車
+                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                        99+
+                    </span>
+                </button>
                 <div class="dropdown text-end">
-                <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                     <span class="fs-2"><i class="bi bi-person rounded-circle"></i></span>
-                </a>
-                <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1" style="">
-                    <li><a class="dropdown-item" href="#">個人資料</a></li>
-                    <li><a class="dropdown-item" href="#">查詢訂單</a></li>
-                    <li><a class="dropdown-item" href="#">賣家中心</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">登出</a></li>
-                </ul>
+                    <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <span class="fs-2"><i class="bi bi-person rounded-circle"></i></span>
+                    </a>
+                    <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1" style="">
+                        <li><a class="dropdown-item" href="#">個人資料</a></li>
+                        <li><a class="dropdown-item" href="#">查詢訂單</a></li>
+                        <li><a class="dropdown-item" href="#">賣家中心</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">登出</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
